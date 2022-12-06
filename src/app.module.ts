c653b21aa3161e88e8f79fa99df718826a29a202
trong <binhtrongcdt1@gmail.com>
2022-12-06 15:48:01 +0700

    setup redis queue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,37 @@
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { EnvModule } from './env/env.module';
+import { BullmqModule } from './bullmq/bullmq.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        ...configService.get('ioredis'),
+      }),
+    }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        ...configService.get('queue'),
+      }),
+    }),
+    BullModule.registerQueue({
+      name: 'demo',
+      // processors: [join(__dirname, 'queue-bull-mq/demo.processor.js')],
+    }),
+    EnvModule,
+    BullmqModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
